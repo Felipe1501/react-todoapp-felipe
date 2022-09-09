876aeb3a3d0cba48f6045c0ff6bee4b8b186b8bb
Felipe Barbosa dos Santos <felipebsantos1501@gmail.com>
2022-09-09 12:18:03 -0300

    aula 127 fix: ODM and REST API creation

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/db_todoApp');
+/*MongoClient.connect("mongodb://localhost:27017/todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });*/
