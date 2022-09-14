de356fe82ec38bf25702de5da858acd10adac6dd
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-14 11:43:49 -0300

    aula 154 add: hotkeys

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,36 +2,47 @@ import React from "react";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
-export default props => (
-    <div role='form' className='todoForm'>
+export default props => {
+    const keyHandler = (e) => {
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape'){
+            props.handleClear()
+        }
+    }
 
-
-            <Grid cols='12 9 10'>
-                    <input 
-                    id="description" 
-                    className="form-control"
-                    placeholder="adicione uma tarefa"
-                    onChange={props.handleChange}
-                    value={props.description} 
+   return (
+        <div role='form' className='todoForm'>
+    
+    
+                <Grid cols='12 9 10'>
+                        <input 
+                        id="description" 
+                        className="form-control"
+                        placeholder="adicione uma tarefa"
+                        onChange={props.handleChange}
+                        onKeyUp={keyHandler}
+                        value={props.description} 
+                        />
+                </Grid>
+    
+                <Grid cols='12 3 2'>
+                    <IconButton 
+                        style='primary' 
+                        icon='plus' 
+                        onCLick={props.handleAdd}
                     />
-            </Grid>
-
-            <Grid cols='12 3 2'>
-                <IconButton 
-                    style='primary' 
-                    icon='plus' 
-                    onCLick={props.handleAdd}
-                />
-                <IconButton 
-                    style='info' 
-                    icon='search' 
-                    onCLick={props.handleSearch}
-                />
-                <IconButton 
-                    style='default' 
-                    icon='close' 
-                    onCLick={props.handleClear}
-                />
-            </Grid>
-    </div>
-)
+                    <IconButton 
+                        style='info' 
+                        icon='search' 
+                        onCLick={props.handleSearch}
+                    />
+                    <IconButton 
+                        style='default' 
+                        icon='close' 
+                        onCLick={props.handleClear}
+                    />
+                </Grid>
+        </div>
+    )
+}
