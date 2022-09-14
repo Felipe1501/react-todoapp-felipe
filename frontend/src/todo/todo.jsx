6552ae6d8fe2f0e7677281db3c9e097af443ecf7
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-14 11:09:24 -0300

    aula 150 fix: query and deletion of TODOs

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,13 @@ export default class Todo extends Component {
         this.state = { description: '', list: []}
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.refresh();
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+             .then(resp => this.setState({...this.state, description: '', list: resp.data}));
     }
     
     handleChange(e){
@@ -23,7 +30,12 @@ export default class Todo extends Component {
     handleAdd() {
         const description = this.state.description;
         axios.post(URL, {description})
-             .then(resp => console.log('Funcionou!'));
+             .then(resp => this.refresh());
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+             .then(resp => this.refresh())
     }
 
     render() {
@@ -34,7 +46,10 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}/>
-                <TodoList />
+                <TodoList 
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                    />
             </div>
         )
     }
